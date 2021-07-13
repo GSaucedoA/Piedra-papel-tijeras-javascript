798fc2a5c64cfa1b8f41222ad4e7689fc5e03309
GSaucedo <g.saucedo.kd@gmail.com>
2021-07-13 02:43:32 -0500

    Finished UI and Functionality

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-//Regresa la alternativa de la pc
+//Global Variables
+let playerScore = 0;
+let pcScore = 0;
+let ties = 0;
+const resultsDiv = document.querySelector("#results");
+const playerHTMLScore = document.querySelector("#playerScore");
+const pcHTMLScore = document.querySelector("#pcScore");
+const tiesHTML = document.querySelector("#ties");
+const pcSelection = document.querySelector(".options__pc-img");
+const pcDefaultText = document.querySelector(".options__pc-default");
+//End Global Variables
+
 const computerPlay = () => {
   const randomNumber = Math.floor(Math.random() * 3);
   const options = ["Rock", "Paper", "Scissors"];
@@ -6,8 +17,12 @@ const computerPlay = () => {
 };
 
 const playRound = (computerSelection, playerSelection) => {
-  computerSelection = computerSelection.toLowerCase();
-  playerSelection = playerSelection.toLowerCase();
+  pcDefaultText.classList.add("none");
+  pcSelection.src = `./img/${computerSelection}.png`;
+  pcSelection.classList.remove("none");
+
+  computerSelection = computerSelection.trim().toLowerCase();
+  playerSelection = playerSelection.trim().toLowerCase();
   //draw = 0, player wins = 1, computer wins = 2
   let message = 2;
   if (playerSelection == computerSelection) {
@@ -19,32 +34,51 @@ const playRound = (computerSelection, playerSelection) => {
   ) {
     message = 1;
   }
+
   return message;
 };
-const game = () => {
-  let playerScore = 0;
-  let computerScore = 0;
-  let ties = 0;
-  for (let i = 0; i < 5; i++) {
-    const playerSelection = prompt("Choose from Rock, Scissors or Paper");
-    const computerSelection = computerPlay();
-    const result = playRound(computerSelection, playerSelection);
-    //draw = 0, player wins = 1, computer wins = 2
-    if (result == 1) {
-      playerScore++;
-    } else if (result == 2) {
-      computerScore++;
-    } else {
-      ties++;
-    }
+
+function updateScore(result) {
+  if (result === 0) {
+    resultsDiv.textContent = "Round tied";
+    tiesHTML.textContent = ++ties;
+  } else if (result === 1) {
+    resultsDiv.textContent = "You win this round!";
+    playerHTMLScore.textContent = ++playerScore;
+  } else {
+    resultsDiv.textContent = "PC wins this round!";
+    pcHTMLScore.textContent = ++pcScore;
   }
-  let message = "Empate";
-  if (playerScore > computerScore) {
-    message = `Ha ganado el jugador Humano quedando ${playerScore} - ${computerScore}`;
-  } else if (computerScore > playerScore) {
-    message = `Ha ganado la computadora quedando ${computerScore} - ${playerScore}`;
+  resultsDiv.classList.remove("none");
+  setTimeout(isGameFinished, 0);
+}
+
+function isGameFinished() {
+  if (playerScore === 5) {
+    alert("Congratulations, you win the game");
+    resetGame();
+  } else if (pcScore === 5) {
+    alert("Bad luck!, PC wins the game");
+    resetGame();
   }
-  message += `, y ha habido un total de ${ties} empates`;
-  return message;
-};
-console.log(game());
+}
+
+function resetGame() {
+  playerScore = 0;
+  pcScore = 0;
+  ties = 0;
+  resultsDiv.textContent = "";
+  playerHTMLScore.textContent = 0;
+  pcHTMLScore.textContent = 0;
+  tiesHTML.textContent = 0;
+  pcDefaultText.classList.remove("none");
+  pcSelection.classList.add("none");
+  resultsDiv.classList.add("none");
+}
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener("click", function () {
+    updateScore(playRound(computerPlay(), this.textContent));
+  });
+});
